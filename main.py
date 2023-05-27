4e3c2450a036431116d04edacb8484f6f37e0ddd
Henrique-Arau <penielh43@gmail.com>
2023-05-26 21:47:47 -0300

    Criando o site com flask

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from flask import Flask
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Beleza, Meu 1º site está no ar!!!!!!!!!!! To sabendo muito'
+
+
+@app.route('/contato')
+def contato():
+    return 'Qualquer dúvida mande um e-mail pata listavip@hashtagtreinamentos.com'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 
