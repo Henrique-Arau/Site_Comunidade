e535c298bcc6c93bf101368378c5f886a03c663a
Henrique-Arau <penielh43@gmail.com>
2023-05-31 22:09:42 -0300

    formulario - Login 2

--- forms.py
+++ forms.py
@@ -2,9 +2,15 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
 
-class FormCriarConta():
+class FormCriarConta(FlaskForm):
     username = StringField('Nome de usuário')
     email = StringField('E-mail')
     senha = PasswordField('Senha')
     confirmacao = PasswordField('Confirmação da senha')
     botao_submit_criarconta = SubmitField('Criar Conta')
+
+
+class FormLogin(FlaskForm):
+    email = StringField('E-mail')
+    senha = PasswordField('Senha')
+    botao_submit_criarconta = SubmitField('Fazer Login')
