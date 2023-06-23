d420bb69f1de30172e0587c2d1433347d51406c1
Henrique-Arau <penielh43@gmail.com>
2023-06-23 12:51:22 -0300

    Criptografando a senha do usuario

--- comunidadeImpressionadora/__init__.py
+++ comunidadeImpressionadora/__init__.py
@@ -1,12 +1,15 @@
-from flask import Flask, Config
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '4fa8fdaf59fcb947808e183c48fcda81'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 app.app_context().push()
+
 from comunidadeImpressionadora import routes
 
 
