74cb22cdaf4c70128d814e9caa4689c0445231f3
Henrique-Arau <penielh43@gmail.com>
2023-07-01 01:10:37 -0300

    Editando formulario de Editar_Perfil

--- comunidadeImpressionadora/routes.py
+++ comunidadeImpressionadora/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for, flash, request
 from comunidadeImpressionadora import app, database, bcrypt
-from comunidadeImpressionadora.forms import FormLogin, FormCriarConta
+from comunidadeImpressionadora.forms import FormLogin, FormCriarConta, FormEditarPerfil
 from comunidadeImpressionadora.models import Usuario
 from flask_login import login_user, logout_user, current_user, login_required
 
@@ -71,4 +71,11 @@ def perfil():
 @login_required
 def criar_post():
     return render_template('criarpost.html')
+
+@app.route('/perfil/editar', methods=['GET', 'POST'])
+@login_required
+def editar_perfil():
+    form = FormEditarPerfil()
+    foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
+    return render_template('editarperfil.html', foto_perfil=foto_perfil, form=form)
 
