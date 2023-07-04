3fb5ee82daf73bc0daa68ab947e23fbefcd0066f
Henrique-Arau <penielh43@gmail.com>
2023-07-04 00:07:30 -0300

    Campo de edição de foto de perfil

--- comunidadeImpressionadora/forms.py
+++ comunidadeImpressionadora/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from comunidadeImpressionadora.models import Usuario
@@ -28,6 +29,7 @@ class FormLogin(FlaskForm):
 class FormEditarPerfil(FlaskForm):
     username = StringField('Nome de Usuario', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
+    foto_perfil = FileField('Atualizar Foto de Perfil', validators=[FileAllowed(['jpg', 'png'])])
     botao_submit_editarperfil = SubmitField('Editar Perfil')
 
     def validate_email(self, email):
