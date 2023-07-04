07173f9ee94b2dfe77e557a7d73bbb8269e3178f
Henrique-Arau <penielh43@gmail.com>
2023-07-04 01:11:37 -0300

    salvar e compactar imagem no banco de dados

--- comunidadeImpressionadora/routes.py
+++ comunidadeImpressionadora/routes.py
@@ -3,6 +3,9 @@ from comunidadeImpressionadora import app, database, bcrypt
 from comunidadeImpressionadora.forms import FormLogin, FormCriarConta, FormEditarPerfil
 from comunidadeImpressionadora.models import Usuario
 from flask_login import login_user, logout_user, current_user, login_required
+import secrets
+import os
+from PIL import Image
 
 lista_usuarios = ['Henrique', 'João', 'Alon', 'Alessandra', 'Amanda']
 
@@ -72,6 +75,21 @@ def perfil():
 def criar_post():
     return render_template('criarpost.html')
 
+def salvar_imagem(imagem):
+    # adicionar o codigo
+    codigo = secrets.token_hex(8)
+    nome, extensao = os.path.splitext(imagem.filename)
+    nome_arquivo = nome + codigo + extensao
+    caminho_completo = os.path.join(app.root_path, 'static/fotos_perfil', nome_arquivo)
+    # reduzir a imagem
+    tamanho = (400, 400)
+    imagem_reduzida = Image.open(imagem)
+    imagem_reduzida.thumbnail(tamanho)
+    # salvar a imagem
+    imagem_reduzida.save(caminho_completo)
+
+    return nome_arquivo
+
 @app.route('/perfil/editar', methods=['GET', 'POST'])
 @login_required
 def editar_perfil():
@@ -79,6 +97,10 @@ def editar_perfil():
     if form.validate_on_submit():
         current_user.email = form.email.data
         current_user.username = form.username.data
+        if form.foto_perfil.data:
+            # adicionar um codigo aleatorio no nome da imagem
+            nome_imagem = salvar_imagem(form.foto_perfil.data)
+            current_user.foto_perfil =nome_imagem
         database.session.commit()
         flash('Perfil atualizado com Sucesso', 'alert-success')
         return redirect(url_for('perfil'))
