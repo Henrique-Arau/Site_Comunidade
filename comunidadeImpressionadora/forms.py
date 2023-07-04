89196946312878adbefdd094d2f32e632a2a3217
Henrique-Arau <penielh43@gmail.com>
2023-07-03 23:42:11 -0300

    Funcionalidade do Formulario de editar Perfil

--- comunidadeImpressionadora/forms.py
+++ comunidadeImpressionadora/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from comunidadeImpressionadora.models import Usuario
+from flask_login import current_user
 
 
 class FormCriarConta(FlaskForm):
@@ -28,3 +29,10 @@ class FormEditarPerfil(FlaskForm):
     username = StringField('Nome de Usuario', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     botao_submit_editarperfil = SubmitField('Editar Perfil')
+
+    def validate_email(self, email):
+        # verificar se o cara mudou de email
+        if current_user.email != email.data:
+            usuario = Usuario.query.filter_by(email=email.data).first()
+            if usuario:
+                raise ValidationError('Já existe um usuário com esse e-mail. cadastre outro e-mail')
